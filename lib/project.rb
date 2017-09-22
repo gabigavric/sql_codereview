df12661a7470aa35ca6873b6030c18ce48cde468
gabrijela <gabigavric@gmail.com>
2017-09-22 15:28:48 -0700

    not run postgress so i cannot make tests pass, almost done with lib for projects class

--- lib/project.rb
+++ lib/project.rb
@@ -17,11 +17,21 @@ class Project
     projects.push(Project.new({:id => id, :title => title}))
   end
   projects
-end
-# 
-# def save
-#     project = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
-#     @id = project.first.fetch("id").to_i
-# end
+  end
+
+  def ==(another_project)
+      (self.title.==(another_project.title)).&(self.id.==(another_project.id))
+  end
+
+  def save
+      project = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+      @id = project.first.fetch("id").to_i
+  end
+
+  def self.find(id)
+      project = DB.exec("SELECT * FROM projects WHERE id = #{id};")
+      title = project.first.fetch("title")
+      Project.new({:title => title, :id => id})
+  end
 
 end
