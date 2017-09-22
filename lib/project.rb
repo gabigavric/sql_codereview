a9ee9ee3e29b1b6f1f29d005ac8d6b6170f14285
gabrijela <gabigavric@gmail.com>
2017-09-22 11:41:28 -0700

    made all method but gem install bundler and bundle commands arent working so cant test

---
+++ lib/project.rb
@@ -0,0 +1,20 @@
+class Project
+
+  attr_accessor :title
+  attr_reader :id
+
+  def initialize(attributes)
+    @id = attributes[:id]
+    @title = attributes[:title]
+  end
+
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each do |project|
+      id = project.fetch("id").to_i
+      title = project.fetch("title")
+    projects.push(Project.new({:id => id, :title => title}))
+  end
+  projects
+end
