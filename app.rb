3ca86c097ed1511381fc8267904990d1f5cdce5a
gabrijela <gabigavric@gmail.com>
2017-10-02 19:10:38 -0700

    path to update project title made

--- app.rb
+++ app.rb
@@ -25,11 +25,19 @@ post('/projects') do
 end
 
 get('/projects/:id') do
-  @project = Project.find(params[:id].to_i())
+  @project = Project.find(params[:id].to_i)
   erb(:project_info)
 end
 
 get('/projects/:id/edit') do
-  @project = Project.find(params[:id].to_i())
+  @project = Project.find(params[:id].to_i)
+  erb(:edit)
+end
+
+post('/projects/:id/edit') do
+  title = params['title']
+  @project_id = params[:id]
+  @project = Project.find(params["id"].to_i)
+  @project.update({:title => title})
   erb(:edit)
 end
